0b8e8f27ac991006f7f6a9c8bbf85680aea2d0d3
Maxwell Kwesi Ehiawey <72410727+MaxwellKwesiEhiawey@users.noreply.github.com>
2022-12-10 19:44:53 +0000

    logger framework completed

---
+++ second-problem-solution/UserDefault.swift
@@ -0,0 +1,26 @@
+//
+//  UserDefault.swift
+//  second-problem-solution
+//
+//  Created by Maxwell Ehiawey on 10/12/2022.
+//
+
+import Foundation
+
+class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+    
+    private init() {}
+    
+    func setValue(value: Any, forKey key: String) {
+        let defaults = UserDefaults.standard
+        defaults.set(value, forKey: key)
+        defaults.synchronize()
+    }
+    
+    func value(forKey key: String) -> Any? {
+        let defaults = UserDefaults.standard
+        return defaults.value(forKey: key)
+    }
+}
+let userDefaults = UserDefaultsManager.shared
--- second-problem-solution/second-problem-solution/ContentView.swift
+++ second-problem-solution/second-problem-solution/ContentView.swift
@@ -1,20 +1,26 @@
-//
-//  ContentView.swift
-//  second-problem-solution
-//
-//  Created by Maxwell Ehiawey on 29/11/2022.
-//
+    //
+    //  ContentView.swift
+    //  second-problem-solution
+    //
+    //  Created by Maxwell Ehiawey on 29/11/2022.
+    //
 
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Text("Hello, world!")
         }
         .padding()
         .onAppear(){
-            AppLoggerClass.shared.detailedLogs("Logging example")
+            userDefaults.setValue(value: "Maxwell:1038443", forKey: "userID")
+            
+            Logger.shared.configure(tag: "dedugging-logs", environment: .debugging)
+            
+            Logger.shared.log("newly created logs", severity: .veryHigh)
+            
         }
     }
 }
