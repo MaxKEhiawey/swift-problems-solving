4997cf5cfed42d9c4d3d66a115715985988c6191
Maxwell Kwesi Ehiawey <72410727+MaxwellKwesiEhiawey@users.noreply.github.com>
2023-07-18 22:28:37 +0000

    added quadratic equation formula with test

--- AgentApp/AgentAppTests/AgentAppTests.swift
+++ AgentApp/AgentAppTests/AgentAppTests.swift
@@ -53,4 +53,24 @@ class CalculatorTests: XCTestCase {
             }
         }
     }
+    
+    func testSolveQuadraticEquation() {
+            // Given
+        let calculator = Calculator()
+
+            // Test case 1: Two distinct real roots
+        let roots1 = calculator.solveQuadraticEquation(a: 2, b: -7, c: 3)
+        XCTAssertEqual(roots1.0, 3.0)
+        XCTAssertEqual(roots1.1, 0.5)
+
+            // Test case 2: One real root
+        let roots2 = calculator.solveQuadraticEquation(a: 1, b: -6, c: 9)
+        XCTAssertEqual(roots2.0, 3.0)
+        XCTAssertNil(roots2.1)
+
+            // Test case 3: No real roots
+        let roots3 = calculator.solveQuadraticEquation(a: 2, b: 1, c: 3)
+        XCTAssertNil(roots3.0)
+        XCTAssertNil(roots3.1)
+    }
 }
